cb5aa9a910d76a5f1f31cd87d8979b09acf0fcfd
HTT <641571835@qq.com>
2022-12-13 11:58:52 +0800

    修复打包失败

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.7.20"
     kotlin("plugin.serialization") version "1.7.20"
-    id("org.jetbrains.compose") version "1.2.0"
+    id("org.jetbrains.compose") version "1.2.1"
 }
 
 group = "com.lookupcat"
@@ -18,6 +18,12 @@ repositories {
     maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
 }
 
+buildscript {
+    dependencies {
+//        classpath("com.guardsquare:proguard-gradle:7.1.0")
+    }
+}
+
 dependencies {
     implementation(compose.desktop.currentOs)
     implementation("moe.sdl.yabapi:yabapi-core-jvm:0.11.1")
@@ -27,15 +33,11 @@ dependencies {
     implementation("org.jetbrains.compose.material:material-icons-extended-desktop:1.2.0")
     implementation("ch.qos.logback:logback-classic:1.4.5")
     implementation("org.slf4j:slf4j-api:2.0.5")
-    compileOnly("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.0")
-    compileOnly("org.jetbrains.kotlinx:atomicfu:0.18.3")
+    implementation("org.jetbrains.kotlinx:atomicfu:0.18.3")
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "17"
-//    kotlinOptions.freeCompilerArgs += "dontwarn org.jetbrains.skiko.*"
-//    kotlinOptions.freeCompilerArgs += "dontwarn ch.qos.logback.*"
-//    kotlinOptions.freeCompilerArgs = listOf("-dontwarn")
+    kotlinOptions.jvmTarget = "11"
 }
 
 compose.desktop {
@@ -45,17 +47,24 @@ compose.desktop {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
             packageName = "bilibili-announcer"
             packageVersion = "1.0.0"
+            val resources = project.file("src/main/resources")
             windows {
                 macOS {
-                    iconFile.set(project.file("src/main/resources/icon.png"))
+                    iconFile.set(resources.resolve("icon.icns"))
                 }
                 windows {
-                    iconFile.set(project.file("src/main/resources/icon.png"))
+                    iconFile.set(resources.resolve("icon.ico"))
+                    shortcut = true
+                    menu = true
                 }
                 linux {
-                    iconFile.set(project.file("src/main/resources/icon.png"))
+                    iconFile.set(resources.resolve("icon.png"))
                 }
             }
+        }
+        // 当前compose版本启用proguard存在一些未知问题: https://github.com/JetBrains/compose-jb/issues/2393
+        buildTypes.release.proguard {
+            isEnabled.set(false)
         }
     }
 }
